e3fe2ccdf0eebe47a4d1845e48a286d64942c88c
Marc Söhn <marcsoehn@gmail.com>
2026-02-14 19:56:02 +0100

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+
+
+from agents import Agent, Runner, function_tool
+
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+@function_tool
+def get_weather(city: str) -> str:
+    return f"The weather in {city} is sunny."
+
+
+agent = Agent(
+    name="Hello world",
+    instructions="You are a helpful agent.",
+    tools=[get_weather],
+)
+
+
+async def main():
+    result = await Runner.run(agent, input="What's the weather in Tokyo?")
+    print(result.final_output)
+    # The weather in Tokyo is sunny.
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
